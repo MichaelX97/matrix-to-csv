55223c15dd9075dfd3e556a038d17136816ba5a0
Hessian <1139581191@qq.com>
2017-12-05 08:46:01 +0800

    Update wqe.py

--- wqe.py
+++ wqe.py
@@ -1,1 +1,58 @@
-import matplotlib.pyplot as plt  <br>import  numpy as np<br>from scipy import optimize<br>data= np.loadtxt(open("C:/Users/xhs/Desktop/123/kb.csv","rb"),delimiter=",",skiprows=0)<br>datax = data[:,0]<br>datay = data[:,1]<br>n=20<br>conversion=0.1<br>GENERATION_COST1=0.03<br>GENERATION_COST2=0.0860<br>penalty=5<br>Y=np.zeros(n)<br>Y0=np.zeros(n)<br>Y1=np.zeros(n)<br>Y2=np.zeros(n)<br>COST=np.zeros(n)<br>for i in range(n):<br>    GENERATION_COST1 = GENERATION_COST1+0.003<br>    def mon(x3):<br>        M = x3 * datax * conversion<br>        return M<br><br>    def cf(A, x1):<br>        M = (datay * 1000 - A - x1) * GENERATION_COST2<br>        for i in range(8760):<br>            if M[i] < 0:<br>                M[i] = 0<br>        return M<br><br>    def R_cf(A, x1, x2):<br>        M = (datay * 1000 - A - x1 - x2) * (penalty - GENERATION_COST2)<br>        for i in range(8760):<br>            if M[i] < 0:<br>                M[i] = 0<br>        return M<br>    <br>    def IC(x1):<br>        M = x1 * GENERATION_COST1<br>        return M<br>    <br>    def f(x):<br>        A =mon(x[2])<br>        Y1 =cf(A, x[0])<br>        Y2 =R_cf(A, x[0], x[1])<br>        Y3 =IC(x[0])<br>        cost = 0<br>        for i in range(8760):<br>            cost = cost + Y1[i] + Y2[i] + Y3<br>        cost = cost + x[0] * 150 + 2.5 * x[1] + x[2] * 50<br>        return cost<br>    result = optimize.fmin_l_bfgs_b(f, np.array([1, 1, 1]), approx_grad=1,bounds=((0, 10000000), (0, 100000000), (0, 10000000)))<br>    X = result[0]<br>    COST = result[1]<br>    Y0[i]=X[0]<br>    Y1[i]=X[1]<br>    Y2[i]=X[2]<br>    Y[i]=X[2]/X[1]<br>plt.plot(range(n), COST,'g')<br>plt.plot(range(n), Y0,'b')<br>plt.plot(range(n), Y2,'r')<br>plt.plot(range(n), Y1,'y')  <br>plt.show<br>
+import matplotlib.pyplot as plt
+import  numpy as npfrom scipy 
+import optimizedata= np.loadtxt(open("C:/Users/xhs/Desktop/123/kb.csv","rb"),delimiter=",",skiprows=0)
+datax = data[:,0]
+datay = data[:,1]
+n=20
+conversion=0.1
+GENERATION_COST1=0.03
+GENERATION_COST2=0.0860
+penalty=5
+Y=np.zeros(n)
+Y0=np.zeros(n)
+Y1=np.zeros(n)
+Y2=np.zeros(n)
+COST=np.zeros(n)
+for i in range(n):
+  GENERATION_COST1 = GENERATION_COST1+0.003
+  def mon(x3):
+    M = x3 * datax * conversion
+    return M
+    def cf(A, x1):
+      M = (datay * 1000 - A - x1) * GENERATION_COST2
+      for i in range(8760):
+        if M[i] < 0:
+          M[i] = 0
+          return M
+    def R_cf(A, x1, x2):
+      M = (datay * 1000 - A - x1 - x2) * (penalty - GENERATION_COST2)
+      for i in range(8760):
+        if M[i] < 0:
+          M[i] = 0
+          return M
+      def IC(x1):
+        M = x1 * GENERATION_COST1
+        return M
+      
+      def f(x):
+        A =mon(x[2])
+        Y1 =cf(A, x[0])
+        Y2 =R_cf(A, x[0], x[1])
+        Y3 =IC(x[0])
+        cost = 0
+        for i in range(8760):
+          cost = cost + Y1[i] + Y2[i] + Y3
+          cost = cost + x[0] * 150 + 2.5 * x[1] + x[2] * 50
+          return cost
+      result = optimize.fmin_l_bfgs_b(f, np.array([1, 1, 1]), approx_grad=1,bounds=((0, 10000000), (0, 100000000), (0, 10000000)))
+      X = result[0]
+      COST = result[1]
+      Y0[i]=X[0]
+      Y1[i]=X[1]
+      Y2[i]=X[2]
+      Y[i]=X[2]/X[1]
+plt.plot(range(n), COST,'g')
+plt.plot(range(n), Y0,'b')
+plt.plot(range(n), Y2,'r')
+plt.plot(range(n), Y1,'y')
+plt.show
